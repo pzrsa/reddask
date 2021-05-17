4dce7d636b9be761547bfa726efbdead24eb5909
Parsa <parsamesg@gmail.com>
2021-05-17 22:25:15 +0100

    dummy form

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+import os
+
+from forms import MyForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 
 @app.route('/')
@@ -10,4 +15,7 @@ def home():
 
 @app.route('/register')
 def register():
-    return render_template('register.html')
+
+    form = MyForm()
+
+    return render_template('register.html', form=form)
