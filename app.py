897d9d54a6e5f3c7fe71fdff63e54a44def9cc12
Parsa <parsamesg@gmail.com>
2021-06-01 22:30:31 +0100

    log user in if validated

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, flash, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import login_user
+from werkzeug.security import check_password_hash
+from models import User
 import os
 
 
@@ -7,6 +11,7 @@ from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+bcrypt = Bcrypt()
 
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
@@ -33,5 +38,14 @@ def register():
 def login():
 
     form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash(f"login successful!")
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check the email and password.', 'danger')
 
     return render_template('login.html', form=form)
