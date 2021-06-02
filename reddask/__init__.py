899cd01b49cf1c9ac656fa3d0d315006883ee6a9
Parsa <parsamesg@gmail.com>
2021-06-02 10:28:26 +0100

    add flask login

--- reddask/__init__.py
+++ reddask/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 import os
@@ -6,8 +7,8 @@ import os
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
-bcrypt = Bcrypt()
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
